a282a3987ab488de34d4395e037b36a3d6d8aeeb
SZ <85321851+mathemate@users.noreply.github.com>
2023-11-17 17:04:24 +0000

    Created InputEditor, a better input that interactive and worked on the login page

---
+++ src/lib/InputEditor.svelte
@@ -0,0 +1,49 @@
+<script >
+
+    export let type = "";
+    export let tailwind = "";
+    export let inside = "";
+
+    export let utext = "text-[2px] top-0	 ";
+
+
+    let css = "absolute opacity-60"
+    let text = ""
+
+    function handleInputChange(event) {
+    text = event.target.value;
+
+    console.log("text has been changed to:" + text)
+
+    if ( text.length > 0) {
+        css = "absolute transition-all duration-300  opacity-60 " + utext
+    }
+
+    else {
+        css = "absolute opacity-60"
+    }
+  }
+
+</script>
+
+
+
+<div class={tailwind+ " relative "}>
+
+    <input 
+        type = {type}
+        class = "absolute bg-transparent focus:border-transparent focus:outline-none z-40"
+        on:input={handleInputChange}    
+    />
+
+    <div class={css}>
+        {inside}
+    </div>
+
+    <div class="opacity-0">
+        {inside}
+    </div>
+
+</div>
+
+
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,24 +1,27 @@
 <script >
-    const inputClass = "rounded-full bg-gray-800 border-white border text-white px-4 py-2"
+    import InputEditor from "$lib/InputEditor.svelte"
+    const inputClass = "w-full rounded-lg bg-gray-800 border-white border text-white px-8 py-3 text-md"
 </script>
 
-<form class="flex flex-col justify-center space-y-4 bg-black px-64 py-56">
+<form class="flex flex-col items-center  space-y-4 bg-black px-32 py-32 w-1/2 relative ">
+
+    <img src = "https://drive.google.com/uc?export=view&id=1-hNKf_ceozK7WtQybUeY9l-BiXfDdK1o" 
+    class = " z-20 w-32 h-32 rounded-full justify-center border border-white p-2" alt="logo"/>
 
     <div class="text-white text-xl font-bold justify-center flex mb-8">
-        Login to West-Computing-Club
+        Login to WC3
     </div>
 
-    <input class = {inputClass} placeholder="User-ID"/>
+    <InputEditor tailwind = {inputClass} inside="User-ID" utext="text-[2px] top-[0.5px]"/>
 
+    <InputEditor tailwind = {inputClass} inside = "Password" type="password" utext="text-[2px] top-[0.5px]"/>
 
-    <input class = {inputClass} placeholder="Password" type="password"/>
-
-    <div class = "flex text-sm text-blue-200 hover:text-blue-400 hover:cursor-pointer justify-end" >
+    <div class = "w-full flex text-sm text-blue-200 hover:text-blue-400 hover:cursor-pointer justify-end" >
         Forgot password
     </div>
 
 
-    <button class = "bg-green-400 p-4 rounded-md" > 
+    <button class = "bg-green-400 p-4 rounded-md w-full" > 
         Login
     </button>
 
