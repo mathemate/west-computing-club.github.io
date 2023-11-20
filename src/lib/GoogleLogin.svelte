7a8591b1b19fe394424261b7e82a018d492086e9
SZ <85321851+mathemate@users.noreply.github.com>
2023-11-20 20:27:46 +0000

    Google Login Button

---
+++ src/lib/GoogleLogin.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+
+<script src="https://accounts.google.com/gsi/client" async></script>
+
+</svelte:head>
+
+<div class="w-full p-10 flex items-center flex-col ">
+
+<div id="g_id_onload"
+     data-client_id="f"
+     data-context="signin"
+     data-ux_mode="popup"
+     data-login_uri="f"
+     data-auto_prompt="false">
+</div>
+
+<div class="g_id_signin"
+     data-type="standard"
+     data-shape="rectangular"
+     data-theme="filled_blue"
+     data-text="signup_with"
+     data-size="large"
+     data-logo_alignment="left"
+     data-width="80">
+</div>
+</div>
+
+
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -1,31 +1,49 @@
 <script >
     import InputEditor from "$lib/InputEditor.svelte"
     const inputClass = "w-full rounded-lg bg-gray-800 border-white border text-white px-8 py-3 text-md"
+
+
+    import GoogleLogin from "$lib/GoogleLogin.svelte";
 </script>
 
-<form class="flex flex-col items-center  space-y-4 bg-black px-32 py-32 w-1/2 relative ">
+<div class="w-full flex flex-row">
 
-    <img src = "https://drive.google.com/uc?export=view&id=1-hNKf_ceozK7WtQybUeY9l-BiXfDdK1o" 
-    class = " z-20 w-32 h-32 rounded-full justify-center border border-white p-2" alt="logo"/>
 
-    <div class="text-white text-xl font-bold justify-center flex mb-8">
-        Login to WC3
+
+    <form class="flex flex-col items-center  space-y-4 bg-black px-32 py-32 w-1/2 relative ">
+
+        <img src = "https://drive.google.com/uc?export=view&id=1-hNKf_ceozK7WtQybUeY9l-BiXfDdK1o" 
+        class = " z-20 w-32 h-32 rounded-full justify-center border border-white p-2" alt="logo"/>
+
+        <div class="text-white text-xl font-bold justify-center flex mb-8">
+            Login to WC3
+        </div>
+
+        <InputEditor tailwind = {inputClass} inside="User-ID" utext="text-[2px] top-[0.5px]"/>
+
+        <InputEditor tailwind = {inputClass} inside = "Password" type="password" utext="text-[2px] top-[0.5px]"/>
+
+        <div class = "w-full flex text-sm text-blue-200 hover:text-blue-400 hover:cursor-pointer justify-end" >
+            Forgot password
+        </div>
+
+
+        <button class = "bg-green-400 p-4 rounded-md w-full" > 
+            Login
+        </button>
+
+
+
+
+    </form>
+
+    <div class = "w-1/2 bg-black items-center flex flex-col py-40  ">
+
+        <div class="text-white text-xl font-bold">
+            Single Sign On
+
+        </div>
+        <GoogleLogin />
     </div>
 
-    <InputEditor tailwind = {inputClass} inside="User-ID" utext="text-[2px] top-[0.5px]"/>
-
-    <InputEditor tailwind = {inputClass} inside = "Password" type="password" utext="text-[2px] top-[0.5px]"/>
-
-    <div class = "w-full flex text-sm text-blue-200 hover:text-blue-400 hover:cursor-pointer justify-end" >
-        Forgot password
-    </div>
-
-
-    <button class = "bg-green-400 p-4 rounded-md w-full" > 
-        Login
-    </button>
-
-
-
-
-</form>
+</div>
