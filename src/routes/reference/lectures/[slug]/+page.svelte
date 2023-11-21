606e668fe4466c5a94acece8038b993ce0286010
SZ <85321851+mathemate@users.noreply.github.com>
2023-11-21 17:10:18 +0000

    Update to link to open in new tab

---
+++ src/routes/reference/lectures/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-center w-full py-20">
+
+<iframe 
+src="https://docs.google.com/presentation/d/e/2PACX-1vREultQ7lfVlY-484ok9YuHdviJqX_CMICARLY7FoWttV6nSDAL1WEVjaV3L3revWYMwa2G08OJStGO/embed?start=false&loop=false&delayms=10000" 
+frameborder="0" width="900" height="600" 
+allowfullscreen="true" 
+mozallowfullscreen="true" webkitallowfullscreen="true"
+title="Lecture"></iframe>
+
+</div>
